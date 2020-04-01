a4d40dc9e2acd23585c545ad5a584a50b843b1f1
Casper <me@casperengelmann.com>
2020-04-01 06:45:39 +0200

    added execution time checker for precision

--- src/CronMs.php
+++ src/CronMs.php
@@ -10,28 +10,68 @@ class CronMs
 {
     const MINUTE = 60000;
 
-    protected Closure $fn;
-
     public int $ms;
+    
+    public float $time_limit;
+
+    public bool $unsafe;
+    
+    protected Closure $fn;
 
     public Carbon $start;
 
-    public int $startTimeStamp;
+    public int $start_timestamp;
 
-    public function __construct(int $ms, callable $fn)
-    {
+    protected float $execution_time = 0;
+
+    public function __construct(
+        int $ms,
+        $time_limit,
+        $fn,
+        bool $unsafe
+    ) {
+        /**
+         * Swap $time_limit and $fn if $time_limit is non-numeric
+         * 
+         * This allows the second parameter to be $fn, thus removing
+         * the need to define a $time_limit
+         */
+        if (! is_numeric($time_limit)) {
+            [$fn, $time_limit] = [$time_limit, $fn];
+        }
+
         $this->ms = $ms;
+
+        $this->time_limit = $time_limit ?: 60000;
+
+        $this->unsafe = $unsafe;
 
         $this->fn = Closure::fromCallable($fn);
 
         $this->start = Carbon::now();
         
-        $this->startTimeStamp = $this->start->timestamp;
+        $this->start_timestamp = $this->start->timestamp;
     }
 
-    public static function fromMs(int $ms, callable $fn, $run_immediately = true): self
-    {
-        $cron = new self($ms, $fn);
+    /**
+     * Returns an instance of CronMs
+     * @param int $ms
+     * @param float|callable $time_limit
+     * @param callable|null $fn
+     * @param bool $run_immediately
+     * @param bool $unsafe
+     * @return self
+     */
+    public static function fromMs(
+        int $ms,
+        $time_limit = null,
+        $fn = null,
+        $run_immediately = true,
+        $unsafe = false
+    ): self {
+        $cron = new self($ms, $time_limit, $fn, $unsafe);
+
+        $cron->checkUnsafe();
 
         if ($run_immediately) {
             $cron->run();
@@ -40,9 +80,25 @@ class CronMs
         return $cron;
     }
 
-    public static function fromSeconds(float $seconds, callable $fn, $run_immediately = true): self
-    {
-        $cron = new self($seconds * 1000, $fn);
+    /**
+     * Returns an instance of CronMs
+     * @param int $ms
+     * @param float|callable $time_limit
+     * @param callable|null $fn
+     * @param bool $run_immediately
+     * @param bool $unsafe
+     * @return self
+     */
+    public static function fromSeconds(
+        float $seconds,
+        $time_limit = null,
+        $fn = null,
+        $run_immediately = true,
+        $unsafe = false
+    ): self {
+        $cron = new self($seconds * 1000, $time_limit, $fn, $unsafe);
+
+        $cron->checkUnsafe();
 
         if ($run_immediately) {
             $cron->run();
@@ -58,16 +114,47 @@ class CronMs
         $division = self::MINUTE / $this->ms;
 
         for ($i = 0; $i < floor($division); $i++) {
+            $time_start = microtime(true);
+
             call_user_func($this->fn, $i);
+
+            $time_end = microtime(true);
 
             if (! m_sleep(
                 self::MINUTE / $division,
-                $this->start->add(60, 'seconds')
+                $this->getLimit()
             )) {
                 break;
+                throw new Exception('Execution exceeded limit.');
+            }
+
+            // When $start has been subtracted from $end, we're left
+            // with the execution time in microseconds
+            // https://www.php.net/manual/en/function.microtime.php
+            // https://stackoverflow.com/a/17035868
+            if ($this->execution_time) {
+                // Compare to last execution time
+                $this->execution_time = ($this->execution_time + ($time_end - $time_start) * 1000) / 2;
+            } else {
+                // Set first execution time
+                $this->execution_time = ($time_end - $time_start) * 1000;
             }
         }
 
         return $this;
+    }
+
+    protected function checkUnsafe()
+    {
+        if (! $this->unsafe && $this->ms < 500) {
+            throw new Exception('$ms is less than 500ms. This may not be the desired behavior. Make sure to turn on the $unsafe flag to proceed.');
+        }
+    }
+
+    protected function getLimit()
+    {
+        return $this->start->copy()
+            ->add($this->time_limit, 'ms')
+            ->sub($this->execution_time, 'ms');
     }
 }
