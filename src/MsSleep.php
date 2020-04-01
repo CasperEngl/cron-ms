6ca76c5207c29b1d4b66b440b95a391efbbb3004
Casper <me@casperengelmann.com>
2020-04-01 04:30:05 +0200

    accepts carbon as argument

--- src/MsSleep.php
+++ src/MsSleep.php
@@ -1,24 +1,25 @@
 <?php
 
-namespace CasperEngl\CronMs;
-
 use Carbon\Carbon;
 
 if (! function_exists('m_sleep')) {
     /**
      * Returns true after sleep, false if the time limit has been reached
      * 
-     * @param int $ms
-     * @param int $time_limit
+     * @param int|float|Carbon $time
+     * @param Carbon $time_limit
      * @return bool
      */
-    function m_sleep(int $ms, int $time_limit = null): bool {
-        if ($time_limit && $time_limit - time() < 0) {
+    function m_sleep($time, Carbon $time_limit = null): bool {
+        if ($time_limit && $time_limit->isPast()) {
             return false;
         }
-        
-        // usleep sleeps for x microseconds
-        usleep($ms * 1000);
+
+        if ($time instanceof Carbon) {
+            usleep($time->diffInMilliseconds(Carbon::now()) * 1000);
+        } else {
+            usleep($time * 1000);
+        }
 
         return true;
     }
